528c9ac1c151ecff07b348b89c0b7237b07856b4
contemplat0r <uldo.forme@gmail.com>
2015-10-30 19:52:27 +0200

    arwu_parser.py full extract information from ARWU ranking table

--- rankings_scraper/arwu_parser.py
+++ rankings_scraper/arwu_parser.py
@@ -32,27 +32,27 @@ def extract_data(html, description):
         university_name = NONE_STR_VALUE
         country = NONE_STR_VALUE
 
-        ## ranking_as_list = row.xpath(ranking_value_xpath)
-        ## name_as_list = row.xpath(university_name_xpath)
-        ## country_as_list = row.xpath(country_xpath)
+        ranking_as_list = row.xpath(ranking_value_xpath)
+        name_as_list = row.xpath(university_name_xpath)
+        country_as_list = row.xpath(country_xpath)
         print '\n', '-' * 10
         print 'Absolute ranking: ', rownum
         ranking = NONE_STR_VALUE
         university_name = NONE_STR_VALUE
         country = NONE_STR_VALUE
-        ## if ranking_as_list != []:
-        ##     ranking = ranking_as_list[0].text
-        ##     print 'Ranking: ', ranking
-        ## if name_as_list != []:
-        ##     university_name = name_as_list[0].text
-        ##     print 'University Name: ', university_name.encode('utf-8')
-        ## if country_as_list != []:
-        ##     country = country_as_list[0].attrib.get('alt')
-        ##     print 'Country: ', country
+        if ranking_as_list != []:
+             ranking = ranking_as_list[0].text
+             print 'Ranking: ', ranking
+        if name_as_list != []:
+            university_name = name_as_list[0].text
+            print 'University Name: ', university_name.encode('utf-8')
+        if country_as_list != []:
+            country = country_as_list[0].attrib.get('title').split()[-1].rstrip('.')
+            print 'Country: ', country
 
         print '\n', '-' * 10, '\n'
 
-        ## ranktable.append({'absolute_ranking' : rownum, 'ranking' : ranking, 'university_name' : university_name, 'country' : country})
+        ranktable.append({'absolute_ranking' : rownum, 'ranking' : ranking, 'university_name' : university_name, 'country' : country})
 
         rownum = rownum + 1
 
@@ -63,13 +63,9 @@ if __name__ == '__main__':
     html = get_saved_html('ARWU_by_Requests-All.html')
     #print html
     table_row_xpath = '//table[@id="UniversityRanking"]/tr[starts-with(@class, "bgf")]'
-    #table_row_xpath = '//table[@id="UniversityRanking"]/tbody/tr'
-
-    ranking_value_xpath = 'td[@class="rank"]/div/div/span[starts-with(@class, "ranking")]'
-    university_name_xpath = 'td[@class="uni"]/div/div/span/a'
-    country_xpath = 'td[@class="country"]/div/img'
-    #table_row_xpath = 'tr'
-    #table_row_xpath = '//tr'
+    ranking_value_xpath = 'td/div'
+    university_name_xpath = 'td[@class="left"]/a'
+    country_xpath = 'td/a[@title]'
     description = {'table_row_xpath' : table_row_xpath, 'ranking_value_xpath' : ranking_value_xpath, 'university_name_xpath' : university_name_xpath, 'country_xpath' : country_xpath}
     ranktable = extract_data(html, description)
     print 'Ranktable extracted'
