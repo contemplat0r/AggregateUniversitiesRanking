81dd48b00bbafde1e2686ba4ed75e0fb60d87849
contemplat0r <uldo.forme@gmail.com>
2015-11-21 20:23:45 +0200

    Begin rewrite get_name_variants function

--- aggregate_universities_ranking/name_matching.py
+++ aggregate_universities_ranking/name_matching.py
@@ -6,69 +6,7 @@ from functools import reduce
 # Добавить "очищенный от вспомогательных слов (артиклей) вариант". Аббревиатуры тогда
 # тоже вычислять два варианта: "очищенную от вспомогательных слов" и "со вспомогательными словами".
 
-anciliary_words_list = ['of', 'the', 'The', 'a', 'A', 'an', 'An']
-
-def entity_name_match(first_name, second_name):
-    name_match = False
-    
-    if (first_name.find(second_name) != -1) or (second_name.find(first_name) != -1):
-        name_match = True
-    else:
-        print 'Name %s and name %s are different' % (first_name, second_name)
-    return name_match
-
-def anciliary_words_filter(name_as_string_list):
-    maybe_is_anciliary_words = False
-    if [word for word in name_as_string_list if len(word) < 4] != []:
-        maybe_is_anciliary_words = True
-    return maybe_is_anciliary_words
-
-def select_anciliary_words(list_of_string_list):
-    def list_containinig_anciliary_words_processor(anciliary_words_list, word):
-        if len(word) < 4 and word not in anciliary_words_list:
-            anciliary_words_list.append(word)
-        return anciliary_words_list
-
-    all_anciliary_words_list = list()
-    for string_list in list_of_string_list:
-        current_anciliary_words_list = reduce(list_containinig_anciliary_words_processor, string_list, list())
-        for anciliary_word in current_anciliary_words_list:
-            if anciliary_word not in all_anciliary_words_list:
-                all_anciliary_words_list.append(anciliary_word)
-    return all_anciliary_words_list
-
-def get_name_part_in_bracket(raw_name_parts_as_str_list):
-    name_part_in_bracket = None
-    abbreviation_in_bracket = None
-    shortname = None
-    for part in raw_name_parts_as_str_list:
-        if part.startswith('(') and part.endswith(')'):
-            name_part_in_bracket = part
-            name_part_in_bracket = name_part_in_bracket.strip('()')
-            if len(name_part_in_bracket) > 1 and name_part_in_bracket.isupper():
-                abbreviation_in_bracket = name_part_in_bracket
-            else:
-                shortname = in_br_part
-            break
-    #if name_part_in_bracket != None: #Эти строчки (эта и две следующих) должны стоять в "головной" ф-ции get_name_variants
-    #    raw_name_parts_as_str_list.remove(br_part)
-    #name_parts = [part.strip('()') for part in raw_name_parts_as_str_list]
-    return abbreviation_in_bracket, shortname
-
-def get_abbreviation_from_fullname(fullname_as_str_list):
-    #name_description['fullname_as_list'] = [part.strip(',') for part in name_parts if part[0].isupper()]
-    fullname_as_str_list = [part.strip(',') for part in fullname_as_str_list if part[0].isupper()]
-    if len(fullname_as_str_list) > 1:
-        abbr_from_fullname = ''
-        for part in fullname_as_str_list:
-            #if part[0].isupper():
-                #abbr_from_fullname = abbr_from_fullname + part[0]
-            abbr_from_fullname = abbr_from_fullname + part[0]
-        #name_description['abbr_from_fullname'] = abbr_from_fullname
-    return abbr_from_fullname
-
-def clean_name_from_anciliary_words(name_as_str_list):
-    return [name_part for name_part in name_as_str_list if name_part not in anciliary_words_list]
+anciliary_words_list = ['of', 'the', 'The', 'a', 'A', 'an', 'An', '&']
 
 def get_name_variants(name_str):
     name_description = {
@@ -116,6 +54,171 @@ def get_name_variants(name_str):
         name_description['fullname'] = fullname_as_string
     name_description['fullname'] = name_str
     return name_description 
+
+
+def entity_name_match(first_name, second_name):
+    name_match = False
+    
+    if (first_name.find(second_name) != -1) or (second_name.find(first_name) != -1):
+        name_match = True
+    else:
+        print 'Name %s and name %s are different' % (first_name, second_name)
+    return name_match
+
+def anciliary_words_filter(name_as_string_list):
+    maybe_is_anciliary_words = False
+    if [word for word in name_as_string_list if len(word) < 4] != []:
+        maybe_is_anciliary_words = True
+    return maybe_is_anciliary_words
+
+def select_anciliary_words(list_of_string_list):
+    def list_containinig_anciliary_words_processor(anciliary_words_list, word):
+        if len(word) < 4 and word not in anciliary_words_list and word.isupper() == False:
+            anciliary_words_list.append(word)
+        return anciliary_words_list
+
+    all_anciliary_words_list = list()
+    for string_list in list_of_string_list:
+        current_anciliary_words_list = reduce(list_containinig_anciliary_words_processor, string_list, list())
+        for anciliary_word in current_anciliary_words_list:
+            if anciliary_word not in all_anciliary_words_list:
+                all_anciliary_words_list.append(anciliary_word)
+    return all_anciliary_words_list
+
+def get_name_part_in_bracket(raw_name_parts_as_str_list):
+    abbreviation_in_bracket = ''
+    shortname = ''
+    for part in raw_name_parts_as_str_list:
+        if part.startswith('(') and part.endswith(')'):
+            name_part_in_bracket = part
+            name_part_in_bracket = name_part_in_bracket.strip('()')
+            if len(name_part_in_bracket) > 1:
+                if name_part_in_bracket.isupper():
+                    abbreviation_in_bracket = name_part_in_bracket
+                else:
+                    shortname = name_part_in_bracket
+            else:
+                print 'Detected name part in bracket with lenght == 1'
+            break
+    return abbreviation_in_bracket, shortname
+
+def get_name_part_in_bracket(raw_name_parts_as_str_list):
+    abbreviation_in_bracket = None
+    shortname = None
+    for part in raw_name_parts_as_str_list:
+        if part.startswith('(') and part.endswith(')'):
+            name_part_in_bracket = part
+            name_part_in_bracket = name_part_in_bracket.strip('()')
+            if len(name_part_in_bracket) > 1:
+                if name_part_in_bracket.isupper():
+                    abbreviation_in_bracket = name_part_in_bracket
+                else:
+                    shortname = name_part_in_bracket
+            else:
+                print 'Detected name part in bracket with lenght == 1'
+            break
+    return abbreviation_in_bracket, shortname
+
+def get_name_part_in_bracket(name_as_str_list):
+    name_part_in_brackets = None
+    for name_part in name_as_str_list:
+        if part.startswith('(') and part.endswith(')'):
+            name_part_in_bracket = name_part
+            break
+    return name_part_in_bracket.strip('()')
+
+def get_abbreviation_from_outside_brackets(name_as_str_list):
+
+def pick_abbreviation_from_fullname(fullname_as_str_list):
+    abbr_from_fullname = None
+    fullname_as_str_list = [part.strip(',') for part in fullname_as_str_list if part[0].isupper()]
+    if len(fullname_as_str_list) > 1:
+        abbr_from_fullname = ''
+        for part in fullname_as_str_list:
+            #if part[0].isupper():
+                #abbr_from_fullname = abbr_from_fullname + part[0]
+            if not part.isupper():
+                abbr_from_fullname = abbr_from_fullname + part[0]
+    return abbr_from_fullname
+
+def remove_anciliary_words_from_name_as_list(name_as_str_list):
+    return [name_part for name_part in name_as_str_list if name_part not in anciliary_words_list]
+
+def get_name_variants(name_str):
+    name_variants = {
+            'fullname_as_list' : None,
+            'fullname_as_list_anciliary_words_removed' : None,
+            'fullname_as_str' : None,
+            'fullname_as_str_anciliary_words_removed' : None,
+            'shortname' : None,
+            'abbreviation_from_bracket' : None,
+            'abbreviation' : None,
+            'abbreviation_pick_from_fullname' : None
+            'abbreviation_pick_from_fullname_minus_anciliary_words' : None
+            'abbreviation_with_ampersand' : None
+            }
+
+    name_as_str_list = name_str.split()
+
+    #abbreviation_from_bracket, shortname = get_name_part_in_bracket(name_as_str_list)
+
+    #if abbreviation_from_bracket != None
+    #    name_variants['abbreviation_from_bracket'] = abbreviation_from_bracket
+    #    name_part_in_bracket = abbreviation_from_bracket
+    #    names_as_str_list.remove(name_part_in_bracket)
+    #    name_parts = [part.strip('()') for part in name_as_str_list]
+    #else if shortname != None
+    #    name_variants['shortname'] = shortname
+    #    name_part_in_bracket = shortname
+    #    names_as_str_list.remove(name_part_in_bracket)
+    #    name_parts = [part.strip('()') for part in name_as_str_list]
+    #else:
+    #    print 'Nonstandart situation! Type name part in bracket not recognized'
+    
+    # Вариант функции get_name_part_in_bracket возвращающей пустые строки.
+    # name_variants['abbreviation_from_bracket'] = abbreviation_from_bracket
+    # name_variants['shortname'] = shortname
+    # names_as_str_list.remove(abbreviation_from_bracket + shortname)
+    # name_parts = [part.strip('()') for part in name_as_str_list]
+
+    name_part_in_bracket = get_name_part_in_bracket(name_as_str_list)
+    if len(name_part_in_bracket) > 1:
+        if name_part_in_bracket.isupper():
+            name_variants['abbreviation_from_bracket'] = name_part_in_bracket
+        else:
+            name_variants['shortname'] = name_part_in_bracket
+        names_as_str_list.remove(name_part_in_bracket)
+        name_parts = [part.strip('()') for part in name_as_str_list]
+    else:
+        print 'Nonstandart situation! Type name part in bracket not recognized'
+
+    abbreviation = None
+    for part in name_parts:
+        if len(part) > 1 and part.isupper():
+            abbreviation = part
+            break
+
+    if abbreviation != None:
+        name_variants['abbreviation'] = abbreviation
+        name_parts.remove(abbreviation)
+
+    if name_parts != []:
+        #name_variants['fullname_as_list'] = [part.strip(',') for part in name_parts if part.istitle()]
+        name_variants['fullname_as_list'] = [part.strip(',') for part in name_parts if part[0].isupper()]
+        if len(name_parts) > 1:
+            abbr_from_fullname = ''
+            for part in name_parts:
+                #if part[0].isupper():
+                    #abbr_from_fullname = abbr_from_fullname + part[0]
+                abbr_from_fullname = abbr_from_fullname + part[0]
+            name_variants['abbr_from_fullname'] = abbr_from_fullname
+        fullname_as_string = name_parts[0]
+        for part in name_parts[1:]:
+            fullname_as_string = fullname_as_string + ' ' + part
+        name_variants['fullname'] = fullname_as_string
+    name_variants['fullname'] = name_str
+    return name_variants 
+
 
 def compare_abbreviations(first_name_description, second_name_description):
     result = False
---
+++ aggregate_universities_ranking/pandas_and_name_matching_exp_2.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+import pandas_and_name_matching_exp_1_clean
+import pandas_and_name_matching_exp_1_clean
+s1 = 'Abc'
+s2 = 'ABC'
+s3 = 'Abc;'
+s1.isupper()
+s2.isupper()
+s3.isupper()
+s3.find('b')
+s3.find('e')
+the_df[the_df['university_name'].find('&') != -1]
+from pandas_and_name_matching_exp_1_clean import *
+the_df[the_df['university_name'].find('&') != -1]
+the_df[the_df['university_name'].map(lambda x: x.find('&') != -1)]
+qs_df[qs_df['university_name'].map(lambda x: x.find('&') != -1)]
+leiden_df[leiden_df['university_name'].map(lambda x: x.find('&') != -1)]
+leiden_df[leiden_df['university_name'].map(lambda x: x.find('&') != -1)]
+s4 = 'A&M'
+s4.isupper()
+s5 = 'A.M'
+s5.isupper()
+s6 = 'a&m'
+s6.islower()
+s6.strip('()')
