71451ca7bce3fd15e58c43b23f6ce1d2e5351b88
contemplat0r <uldo.forme@gmail.com>
2015-12-03 11:52:12 +0200

    Change selected_ranking_names to selected_rankings_names in line if selected_rankings_names != []: to avoid logical error

--- aggregate_universities_ranking/aggregate_ranking_representation/views.py
+++ aggregate_universities_ranking/aggregate_ranking_representation/views.py
@@ -18,11 +18,10 @@ def index(request):
     return HttpResponse('Hello, world. This is first page of aggrgated rank')
 
 def aggregate_universities_ranking_as_table(request):
-    #short_rankings_names_choice_set = [(raiting_name.short_name, raiting_name.short_name) for raiting_name in RaitingName.objects.all()]
-    print 'START_AGGREGATE_YEAR: ', START_AGGREGATE_YEAR
+    print 'Entry in aggregate_universities_ranking_as_table'
     short_rankings_names = [ranking_name.short_name for ranking_name in RankingName.objects.all()]
     short_rankings_names_choice_set = [(short_name, short_name) for short_name in short_rankings_names]
-    print short_rankings_names_choice_set
+    print 'aggregate_universities_ranking_as_table, short_rankings_names_choice_set', short_rankings_names_choice_set
 
     select_rankings_names_form = SelectRankingsNamesAndYear()
     select_rankings_names_form.fields['select_rankings_names_field'].choices = tuple(short_rankings_names_choice_set)
@@ -32,14 +31,12 @@ def aggregate_universities_ranking_as_table(request):
     if request.method == 'POST':
         select_rankings_names_form = SelectRankingsNamesAndYear(request.POST)
         select_rankings_names_form.fields['select_rankings_names_field'].choices = tuple(short_rankings_names_choice_set)
-        #select_rankings_names_form.fields['select_year_field'].choices = (('2014', '2014'), ('2015', '2015',))
-        #select_rankings_names_form.fields['select_year_field'].choices = tuple([(year, year) for year in range(START_AGGREGATE_YEAR, FINISH_AGGREGATE_YEAR + 1)])
         select_rankings_names_form.fields['select_year_field'].choices = tuple([(year, year) for year in range(FINISH_AGGREGATE_YEAR, START_AGGREGATE_YEAR - 1, -1)])
         if select_rankings_names_form.is_valid():
             selected_rankings_names = select_rankings_names_form.cleaned_data.get('select_rankings_names_field')
             selected_year = select_rankings_names_form.cleaned_data.get('select_year_field')
             year_as_datetime = datetime.date(int(selected_year), 1, 1)
-            print 'year date string: ', year_as_datetime.strftime('%Y-%m-%y')
+            print 'aggregate_universities_ranking_as_table, year date string: ', year_as_datetime.strftime('%Y-%m-%y')
         else:
             select_rankings_names_form.fields['select_rankings_names_field'].choices = tuple(short_rankings_names_choice_set)
             select_rankings_names_form.fields['select_year_field'].choices = tuple([(year, year) for year in range(FINISH_AGGREGATE_YEAR, START_AGGREGATE_YEAR - 1, -1)])
@@ -48,15 +45,19 @@ def aggregate_universities_ranking_as_table(request):
         select_rankings_names_form.fields['select_rankings_names_field'].choices = tuple(short_rankings_names_choice_set)
         select_rankings_names_form.fields['select_year_field'].choices = tuple([(year, year) for year in range(FINISH_AGGREGATE_YEAR, START_AGGREGATE_YEAR - 1, -1)])
         selected_year = FINISH_AGGREGATE_YEAR
-        print 'selected_year: ', selected_year, 'type(selected_year): ', type(selected_year)
+        print 'aggregate_universities_ranking_as_table, selected_year: ', selected_year, 'type(selected_year): ', type(selected_year)
 
         year_as_datetime = datetime.date(int(selected_year), 1, 1)
-        print 'year date string: ', year_as_datetime.strftime('%Y-%m-%y')
-     
+        print 'aggregate_universities_ranking_as_table, year date string: ', year_as_datetime.strftime('%Y-%m-%y')
+    
     selected_rankings_descriptions = ranking_descriptions
-    if selected_ranking_names != []:
+    print 'aggregate_universities_ranking_as_table, selected_rankings_descriptions before selection: ', selected_rankings_descriptions
+    if selected_rankings_names != []:
         selected_rankings_descriptions = {ranking_name : ranking_descriptions[ranking_name] for ranking_name in selected_rankings_names if ranking_name in ranking_descriptions.keys()}
+    print 'aggregate_universities_ranking_as_table, selected_rankings_descriptions after selection: ', selected_rankings_descriptions
+
     aggregate_ranking_dataframe = build_aggregate_ranking_dataframe(selected_rankings_descriptions)
+
     ranktable = {'headers' : {'rank' : 'Rank', 'aggregate_rank' : 'Aggregate Rank', 'university_name' : 'University Name'}}
 
     ranktable['headers'].update({rankname.lower() : None for rankname in short_rankings_names})
