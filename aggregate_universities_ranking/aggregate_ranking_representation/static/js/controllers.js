ccd69ade4bb98c5ed733e037812bd4524dfe4e4f
contemplat0r <uldo.forme@gmail.com>
2015-12-26 18:55:21 +0200

    First susses by using angular (controller work right,  work)

--- aggregate_universities_ranking/aggregate_ranking_representation/static/js/controllers.js
+++ aggregate_universities_ranking/aggregate_ranking_representation/static/js/controllers.js
@@ -2,26 +2,9 @@
 
 /* Controllers */
 
-var phonecatControllers = angular.module('aggregaterankingControllers', []);
-aggregaterankingControllers.controller('RankingListCtrl', function($scope, $http) {
-    $http.get('list').success(function(data) {
-        $scope.list = data;
-    });
+var rankingApp = angular.module('rankingApp', []);
+
+rankingApp.controller('RankingListCtrl', function($scope) {
+    $scope.name = 'World';
 });
 
-/*phonecatControllers.controller('RankingListCtrl', ['$scope', 'Phone',
-  function($scope, Phone) {
-    $scope.phones = Phone.query();
-    $scope.orderProp = 'age';
-  }]);
-
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
-    });
-
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    };
-  }]);*/
