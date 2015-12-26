570afc9a1ea9cf55c5f4ed40ca13cda8aea8d4bc
contemplat0r <uldo.forme@gmail.com>
2015-12-26 22:51:37 +0200

    First success by sending HTTP request by AngularJS (controllers.js)

--- aggregate_universities_ranking/aggregate_ranking_representation/static/js/controllers.js
+++ aggregate_universities_ranking/aggregate_ranking_representation/static/js/controllers.js
@@ -4,7 +4,14 @@
 
 var rankingApp = angular.module('rankingApp', []);
 
-rankingApp.controller('RankingListCtrl', function($scope) {
+rankingApp.controller('RankingListCtrl', function($scope, $http) {
     $scope.name = 'World';
+    $scope.rankinglist = [
+        {'name' : 'QS'},
+        {'name' : 'THE'}
+    ];
+    $http.get('api/table?format=json').success(function(data) {
+        $scope.table = data;
+    });
 });
 
