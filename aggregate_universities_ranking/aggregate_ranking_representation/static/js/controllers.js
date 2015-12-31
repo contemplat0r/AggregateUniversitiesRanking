55ac1f67b8039bd623bd3da93507692e8c02d3a1
contemplat0r <uldo.forme@gmail.com>
2015-12-31 20:25:20 +0200

    Fully working AngularJS variant of presentation table, ranking names checklist, and year select list

--- aggregate_universities_ranking/aggregate_ranking_representation/static/js/controllers.js
+++ aggregate_universities_ranking/aggregate_ranking_representation/static/js/controllers.js
@@ -7,12 +7,56 @@ var rankingApp = angular.module('rankingApp', []);
 
 rankingApp.controller('RankingTableCtrl', function($scope, $http) {
     $scope.rankingTable = {headers : [], records : []};
-    var prepareResponseDataToShow = function(responseData, scope) {
-        console.log('Entry in prepareResponseDataToShow');
+    var updateLocalDataByResponse = function(responseData, scope) {
+        console.log('Entry in updateLocalDataByResponse');
         var rankingTable = responseData['ranktable'];
         scope.rankingTable['headers'] = rankingTable[0];
         scope.rankingTable['records'] = rankingTable.slice(1);
+        if (!('yearselect' in scope) && !('rankingchecklist' in scope)) {
+            if (responseData.rankings_names_list != null) {
+                scope.rankingchecklist = [];
+                scope.rankingchecklist = responseData.rankings_names_list.map(function(rankingName) {
+                    return {name : rankingName, value : false};
+                });
+                //console.log('updateLocalDataByResponse, rankingchecklist = ' + scope.rankingchecklist.join(' '));
+            }
+            if (responseData.years_list != null) {
+                //scope.yearselect = responseData.years;
+                //var yearslist = [2014, 2015, 2016];
+                var yearslist = responseData.years_list;
+                yearslist.sort();
+                yearslist.reverse();
+
+                //$scope.rankingchecklist  = [
+                //    {name : 'QS', value : false},
+                //    {name : 'THE', value : true},
+                //    {name : 'NTU', value : false}
+                //];
+
+                $scope.yearselect = {
+                    availableYears : [],
+                    selectedYear : {id : 0, value : 0}
+                };
+                
+                var maxYear = 0;
+                var maxYearId = 0;
+                yearslist.forEach(function(year, i, yearslist) {
+                    $scope.yearselect.availableYears.push({id : i, value : year});
+                    if (year > maxYear) {
+                        maxYear = year;
+                        maxYearId = i;
+                    }
+                });
+                $scope.yearselect.selectedYear.id = maxYearId;
+                $scope.yearselect.selectedYear.value = maxYear;
+            }
+        }
+        else
+        {
+            conslole.log("yearselect in scope or/and rankingchecklist in scope");
+        }
     };
+
     $scope.requestData =  {selectedRankingNames : null, selectedYear : null}
     $scope.retrieveTableData = function(requestData) {
         console.log('retrieveTableData: $scope.requestData.selectedRankingNames: ' + $scope.requestData.selectedRankingNames + ', $scope.requestData.selectedYear: ' + $scope.requestData.selectedYear);
@@ -24,7 +68,7 @@ rankingApp.controller('RankingTableCtrl', function($scope, $http) {
             function successCallback(response) {
                 console.log('retrieveTableData, success: ', response);
                 $scope.table = response.data;
-                prepareResponseDataToShow(response.data, $scope);
+                updateLocalDataByResponse(response.data, $scope);
                 //console.log('$scope.table[0]: ' + $scope.table[0].join(' '));
             },
             function errorrCallback(response) {
@@ -39,7 +83,7 @@ rankingApp.controller('RankingTableCtrl', function($scope, $http) {
 
 
 rankingApp.controller('RankingCheckController', function($scope, $http) {
-    var yearslist = [2014, 2015, 2016];
+    /*var yearslist = [2014, 2015, 2016];
     yearslist.sort();
     yearslist.reverse();
 
@@ -49,7 +93,7 @@ rankingApp.controller('RankingCheckController', function($scope, $http) {
         {name : 'NTU', value : false}
     ];
 
-    $scope.yearsselect = {
+    $scope.yearselect = {
         availableYears : [],
         selectedYear : {id : 0, value : 0}
     };
@@ -57,14 +101,14 @@ rankingApp.controller('RankingCheckController', function($scope, $http) {
     var maxYear = 0;
     var maxYearId = 0;
     yearslist.forEach(function(year, i, yearslist) {
-        $scope.yearsselect.availableYears.push({id : i, value : year});
+        $scope.yearselect.availableYears.push({id : i, value : year});
         if (year > maxYear) {
             maxYear = year;
             maxYearId = i;
         }
     });
-    $scope.yearsselect.selectedYear.id = maxYearId;
-    $scope.yearsselect.selectedYear.value = maxYear;
+    $scope.yearselect.selectedYear.id = maxYearId;
+    $scope.yearselect.selectedYear.value = maxYear;*/
 
     $scope.sendselected = function() {
         console.log('Entry in sendselected');
@@ -77,7 +121,7 @@ rankingApp.controller('RankingCheckController', function($scope, $http) {
             }
         });
         console.log('Sendselected, selectednames: ' + selectednames.join(' '));
-        $scope.requestData = {selectedRankingNames : selectednames, selectedYear : $scope.yearsselect.selectedYear.value}
+        $scope.requestData = {selectedRankingNames : selectednames, selectedYear : $scope.yearselect.selectedYear.value}
         console.log('$scope.requestData.selectedRankingNames: ' + $scope.requestData.selectedRankingNames + ', $scope.requestData.selectedYear: ' + $scope.requestData.selectedYear);
         $scope.retrieveTableData($scope.requestData);
     };
